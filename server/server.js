bffe802e802efffd6bf1259baefecf8786051a19
Anurag Raj <anurag2003raj@gmail.com>
2026-06-23 00:41:05 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors" ;
 import "dotenv/config" ;
 import connectDB from "./config/db.js";
+import authRouter from "./route/authRoutes.js";
 
 connectDB()
 const app = express() 
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/' , (req,res) => res.send("server is running"))
+app.use("/api/auth" , authRouter)
 
 const PORT = process.env.PORT || 5000;
 
